407b211c74defd9b5378e1b0f9b3f2e7196e91f9
GitHub Action <action@github.com>
2021-11-29 07:39:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/mreram/bazaartemplateplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mreram/bazaartemplateplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mreram.bazaartemplateplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mreram.bazaartemplateplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
