ac4b7b8a1feb440f3b50668fa64f57cc8e7b2e73
cafebazaar <mreram@yahoo.com>
2021-12-13 17:33:45 +0330

    Remove base folder

--- src/main/kotlin/com/github/mreram/bazaartemplateplugin/base/Module.kt
+++
@@ -1,8 +0,0 @@
-package com.github.mreram.bazaartemplateplugin.base
-
-data class Module(
-    val name: String,
-    val nameWithoutPrefix: String
-) {
-    override fun toString() = nameWithoutPrefix
-}
